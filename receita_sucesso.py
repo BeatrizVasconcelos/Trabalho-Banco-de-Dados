b27ac01482fbb2eb8144f30a8019d2b9f264e488
Marcos Sombra <markos.vs@live.com>
2018-11-11 02:31:13 -0300

    add avaliação de receitas

--- receita_sucesso.py
+++ receita_sucesso.py
@@ -1,6 +1,6 @@
 from database import Database
 from menus import (menu_inicial, menu_principal)
-from aux import (login, cadastro, get_receitas, insere_receita)
+from aux import (login, cadastro, get_receitas, insere_receita, sim_ou_nao)
 
 
 # linha de execução do programa
@@ -43,7 +43,7 @@ def main():
             # exibe a lista total de receitas
             if(opcao == 1):
                 # imprime a lista de receitas
-                imprime_receitas(db)
+                imprime_receitas(db, usuario[0])
             # caso o usuario escolha a opcao 2
             # exibe menu para inserir receita
             # e insere
@@ -66,7 +66,7 @@ def main():
             # exibe a lista total de receitas
             if(opcao == 1):
                 # imprime a lista de receitas
-                imprime_receitas(db)
+                imprime_receitas(db, usuario[0])
             # caso o usuario escolha a opcao 2
             # altera seus dados
             elif(opcao == 2):
@@ -80,7 +80,7 @@ def get_nome(usuario):
     return ' '.join((usuario[1], usuario[2]))
 
 
-def imprime_receitas(db, *argv):
+def imprime_receitas(db, usuario, *argv):
     # retorna a lista de receitas
     if(len(argv) > 0):
         receitas = get_receitas(db, argv[0])
@@ -111,6 +111,16 @@ def imprime_receitas(db, *argv):
                 " ORDER BY num_passo;".format(receita[0])
             passos = db.select(q)
 
+            # busca avaliações
+            q = "SELECT * FROM avaliacao WHERE id_receita={} ORDER BY id DESC;"
+            q = q.format(receita[0])
+            reviews = db.select(q)
+
+            # checa se o usuario ja avaliou
+            q = "SELECT COUNT(id) FROM avaliacao WHERE id_autor={}" \
+                "AND id_receita={}".format(usuario, receita[0])
+            review_user = (db.select(q))[0][0]
+
             # imprime informações da receita
             print('=======================')
             print('ID: {}'.format(receita[0]))
@@ -119,6 +129,13 @@ def imprime_receitas(db, *argv):
             print('Nível do Autor: {}'.format(autor[2]))
             print('Avaliação Média: {}'.format(receita[2]))
             print('---')
+            print('Última avaliação:')
+            if(len(reviews) > 0):
+                print('Nota: {}'.format(reviews[0][3]))
+                print('Texto: {}'.format(reviews[0][4]))
+            else:
+                print('Nenhuma avaliação disponível')
+            print('---')
             print('Ingredientes:')
             for ing in ingredientes:
                 print('{} - {} {}'.format(ing[0], ing[1], ing[2]))
@@ -126,7 +143,45 @@ def imprime_receitas(db, *argv):
             for n, passo in passos:
                 print('{} - {}'.format(n, passo))
             print('=======================')
+
+            # pergunta se o usuaŕio deseja avaliar a receita
+            if(review_user == 0):
+                print('Deseja avaliar essa receita?')
+                resp = sim_ou_nao()
+                if(resp == 1):
+                    avaliar_receita(db, usuario, receita[0])
         print()
+
+
+# avalia uma receita
+def avaliar_receita(db, autor, rec):
+    # lê uma nota de 1 à 5
+    print('Digite uma nota: (de 1 à 5)')
+    while(True):
+        try:
+            stars = int(input())
+        except ValueError:
+            stars = -1
+        finally:
+            if(stars >= 1 and stars <= 5):
+                break
+            print("Resposta inválida")
+
+    print('Faça um comentário:')
+    comment = input()
+
+    # insere a avaliação na tabela
+    q = "INSERT INTO avaliacao (id_autor,id_receita, num_estrelas, texto)" \
+        " VALUES ({}, {}, {}, '{}');".format(autor, rec, stars, comment)
+    db.execute_query(q)
+    # busca a nova media de estrelas daquela receita
+    q = "SELECT AVG(num_estrelas) FROM avaliacao" \
+        " WHERE id_receita={}".format(rec)
+    media = (db.select(q))[0][0]
+
+    # atualiza a avaliacao media da receita
+    q = "UPDATE receita SET avaliacao_media={} WHERE id={};".format(media, rec)
+    db.execute_query(q)
 
 
 if(__name__ == '__main__'):
